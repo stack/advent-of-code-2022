cb9195533a01ef0b14cbf0c0e19e077437e3a951
Stephen H. Gerstacker <stephen@gerstacker.us>
2022-12-06 07:33:31 -0500

    Day 06 - Both Parts

---
+++ Day 06/main.swift
@@ -0,0 +1,57 @@
+//
+//  main.swift
+//  Day 06
+//
+//  Created by Stephen H. Gerstacker on 2022-12-06.
+//  Copyright © 2022 Stephen H. Gerstacker. All rights reserved.
+//
+
+import Algorithms
+import Foundation
+
+// MARK: - Part 1
+
+func firstPacketMarker(data: String) -> (String, Int) {
+    for (markerIndex, marker) in data.windows(ofCount: 4).enumerated() {
+        let characters = Set(marker)
+        
+        if characters.count == 4 {
+            return (String(marker), markerIndex + 4)
+        }
+    }
+    
+    fatalError("No marker found")
+}
+
+for data in [SampleData1, SampleData2, SampleData3, SampleData4, SampleData5] {
+    let (marker, index) = firstPacketMarker(data: data)
+    
+    print("Sample Packet Marker: \(marker): Index: \(index)")
+}
+
+let (packetMarker, packetIndex) = firstPacketMarker(data: InputData)
+print("Packet Marker: \(packetMarker): Index: \(packetIndex)")
+
+// MARK: - Part 2
+
+func firstMessageMarker(data: String) -> (String, Int) {
+    for (markerIndex, marker) in data.windows(ofCount: 14).enumerated() {
+        let characters = Set(marker)
+        
+        if characters.count == 14 {
+            return (String(marker), markerIndex + 14)
+        }
+    }
+    
+    fatalError("No marker found")
+}
+
+for data in [SampleData6, SampleData7, SampleData8, SampleData9, SampleData10] {
+    let (marker, index) = firstMessageMarker(data: data)
+    
+    print("Sample Message Marker: \(marker): Index: \(index)")
+}
+
+let (messageMarker, messageIndex) = firstMessageMarker(data: InputData)
+print("Message Marker: \(messageMarker): Index: \(messageIndex)")
+
